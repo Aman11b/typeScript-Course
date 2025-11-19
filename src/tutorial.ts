3669ab124327c5f834c81ce971cd809ccec27464
Aman11b <amansingh1100b@gmail.com>
2025-11-19 08:10:01 +0530

    type annotation & type inference

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -8,3 +8,42 @@ console.log("typeScript tutorial");
 //   id: 123,
 // };
 // console.log(someObj);
+
+/************************
+ * TYPE ANNOTATION
+ *************************/
+let awesomeName: string = "shakeAndBake";
+awesomeName = "something...";
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName);
+
+// awesomeName=20;
+// awesomeName=true;
+
+let amount: number = 20;
+amount = 12 - 1;
+// amount="pant"
+
+let isAwesome: boolean = true;
+isAwesome = false;
+// isAwesome=3
+
+/************************
+ * TYPE INFERENCE
+ ************************/
+
+let awesomeName1 = "shakeAndBake";
+awesomeName1 = "somethingINFERENCE";
+awesomeName1 = awesomeName1.toUpperCase();
+console.log(awesomeName1);
+
+// awesomeName=20;
+// awesomeName=true;
+
+let amount1 = 20;
+amount1 = 12 - 1;
+// amount="pant"
+
+let isAwesome1 = true;
+isAwesome1 = false;
+// isAwesome=3
