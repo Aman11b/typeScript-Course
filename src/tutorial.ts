1a0ea886ded12958f940c591e2af5483d662acea
Aman11b <amansingh1100b@gmail.com>
2025-12-30 16:07:16 +0530

    type guard instance of

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -904,3 +904,28 @@ printLength("");
 printLength(null);
 printLength();
 printLength(undefined);
+
+console.log("----instanse of type guard------");
+try {
+  // throw new Error("This is an error");
+  throw "some error";
+} catch (error) {
+  if (error instanceof Error) {
+    console.log(`Caught an Error object: ${error.message}`);
+  } else {
+    console.log("Unknown Error...");
+  }
+}
+
+// check if obj is instance of class
+function checkInput(input: Date | string): string {
+  if (input instanceof Date) {
+    return input.getFullYear().toString();
+  }
+  return input;
+}
+
+const year = checkInput(new Date());
+const randYear = checkInput("2020-05-23");
+console.log(year);
+console.log(randYear);
